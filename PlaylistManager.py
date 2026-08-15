b0b62e3044ca25deb8742a10319e351367a71c4c
hannanicolemendiola <hannanicolemendiola@gmail.com>
2026-08-15 08:50:43 +0000

    Added song library

--- PlaylistManager.py
+++ PlaylistManager.py
@@ -1,3 +1,18 @@
-print("SongSearch")
-#def new_playlist ():
- #   print("+ New Playlist")
+print("SongSearch: Playlist Manager\nEnter 1 to create a playlist")
+def new_playlist():
+    choice = input("Do you want to create a new playlist?\n 1 - Yes\n Any key - No\n")
+    if choice = 1:
+        print
+
+songs = [Queen – Bohemian Rhapsody,
+Michael Jackson – Billie Jean,
+Nirvana – Smells Like Teen Spirit,
+The Police – Every Breath You Take,
+Fleetwood Mac – Dreams,
+Bob Dylan – Like a Rolling Stone,
+Aretha Franklin – Respect,
+The Beatles – Yesterday,
+Marvin Gaye – What’s Going On,
+The Weeknd – Blinding Lights]
+
+new_playlist()
