a74f8d76ec6296a446151ac246769338aaa7767a
hannanicolemendiola <hannanicolemendiola@gmail.com>
2026-08-15 08:59:39 +0000

    Fixed code

--- PlaylistManager.py
+++ PlaylistManager.py
@@ -1,18 +1,20 @@
+songs = ["Queen - Bohemian Rhapsody",
+"Michael Jackson - Billie Jean",
+"Nirvana - Smells Like Teen Spirit",
+"The Police - Every Breath You Take",
+"Fleetwood Mac - Dreams",
+"Bob Dylan - Like a Rolling Stone",
+"Aretha Franklin - Respect",
+"The Beatles - Yesterday",
+"Marvin Gaye - What's Going On",
+"The Weeknd - Blinding Lights"]
+
 print("SongSearch: Playlist Manager\nEnter 1 to create a playlist")
 def new_playlist():
     choice = input("Do you want to create a new playlist?\n 1 - Yes\n Any key - No\n")
-    if choice = 1:
-        print
-
-songs = [Queen – Bohemian Rhapsody,
-Michael Jackson – Billie Jean,
-Nirvana – Smells Like Teen Spirit,
-The Police – Every Breath You Take,
-Fleetwood Mac – Dreams,
-Bob Dylan – Like a Rolling Stone,
-Aretha Franklin – Respect,
-The Beatles – Yesterday,
-Marvin Gaye – What’s Going On,
-The Weeknd – Blinding Lights]
+    if choice == "1":
+        print("Add songs\nFor you\n\n")
+        for song in songs:
+            print(song)
 
 new_playlist()
