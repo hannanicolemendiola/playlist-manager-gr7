74cd63aa7a076229bc941be02594260db6ecbd24
hannanicolemendiola <hannanicolemendiola@gmail.com>
2026-08-15 11:08:49 +0000

    Added playlist list

--- PlaylistManager.py
+++ PlaylistManager.py
@@ -1,20 +1,36 @@
 songs = ["Queen - Bohemian Rhapsody",
-"Michael Jackson - Billie Jean",
-"Nirvana - Smells Like Teen Spirit",
-"The Police - Every Breath You Take",
-"Fleetwood Mac - Dreams",
-"Bob Dylan - Like a Rolling Stone",
-"Aretha Franklin - Respect",
-"The Beatles - Yesterday",
-"Marvin Gaye - What's Going On",
-"The Weeknd - Blinding Lights"]
+        "Michael Jackson - Billie Jean",
+        "Nirvana - Smells Like Teen Spirit",
+        "The Police - Every Breath You Take",
+        "Fleetwood Mac - Dreams",
+        "Bob Dylan - Like a Rolling Stone",
+        "Aretha Franklin - Respect",
+        "The Beatles - Yesterday",
+        "Marvin Gaye - What's Going On",
+        "The Weeknd - Blinding Lights"]
 
-print("SongSearch: Playlist Manager\nEnter 1 to create a playlist")
+playlists = []
+
+def welcome():
+    choice = input("SongSearch: Playlist Manager\nEnter 1 to create a playlist\nEnter 2 to view playlists\nEnter 3 to exit")
 def new_playlist():
-    choice = input("Do you want to create a new playlist?\n 1 - Yes\n Any key - No\n")
-    if choice == "1":
-        print("Add songs\nFor you\n\n")
-        for song in songs:
-            print(song)
+    playlist = input("Enter playlist name: ")
+    playlists.append(new_playlist)
+    print(f"Playlist {playlist} created!")
+    print("Add songs\nFor you\n")
+    for song in songs:
+        print(song)
+    welcome()
 
-new_playlist()
+    if choice == "1":
+        new_playlist()
+    elif choice == "2":
+        for playlist in playlists:
+            print(playlist)
+    elif choice == "3":
+        exit()
+    else:
+        print("Invalid choice!")
+
+while True:
+    welcome()
