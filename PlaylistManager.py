55c1790c8554b68ff15f6da1265fa7308bc30f57
hannanicolemendiola <hannanicolemendiola@gmail.com>
2026-08-17 02:39:57 +0000

    Finished create playlist system

--- PlaylistManager.py
+++ PlaylistManager.py
@@ -12,25 +12,28 @@ songs = ["Queen - Bohemian Rhapsody",
 playlists = []
 
 def welcome():
-    choice = input("SongSearch: Playlist Manager\nEnter 1 to create a playlist\nEnter 2 to view playlists\nEnter 3 to exit")
+    return input("SongSearch: Playlist Manager\nEnter 1 to create a playlist\nEnter 2 to view playlists\nEnter 3 to exit\n")
 def new_playlist():
     playlist = input("Enter playlist name: ")
-    playlists.append(new_playlist)
+    playlists.append(playlist)
     print(f"Playlist {playlist} created!")
     print("Add songs\nFor you\n")
     for song in songs:
         print(song)
-    welcome()
+
+while True:
+    choice = welcome()
 
     if choice == "1":
         new_playlist()
     elif choice == "2":
-        for playlist in playlists:
-            print(playlist)
+        if len(playlists) == 0:
+            print("No playlists yet!")
+        else:
+            for playlist in playlists:
+                print(playlist)
+        input("Press Enter to return to menu")
     elif choice == "3":
         exit()
     else:
         print("Invalid choice!")
-
-while True:
-    welcome()
